1a53bbe32554a08f080c503cb13cf1bb42d0e8bf
Fabiogamboa <150569632+Fabiogamboa@users.noreply.github.com>
2025-06-20 20:53:26 -0500

    v 1.02

--- app.py
+++ app.py
@@ -16,8 +16,9 @@ with app.app_context():
 def index():
     return render_template('index.html')
 
-
-
+@app.route('/catalogo')
+def catalogo():
+    return render_template('catalogo.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
